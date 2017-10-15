c6c94e984de8a0ffe8a33ca07a7557b728a5fcd2
Cryptum169 <ruoyangx@qq.com>
2017-10-15 00:00:02 -0400

    Last Minute

--- voice2text.py
+++ voice2text.py
@@ -7,7 +7,7 @@ import timeout_decorator
 
 class voice2text:
     def __init__(self):
-        self.mic_name = "HDA Intel PCH: ALC3266 Analog (hw:0,0)"
+        self.mic_name = "HDA Intel PCH: ALC892 Analog (hw:1,0)"
         #Sample rate is how often values are recorded
         self.sample_rate = 48000
         #Chunk is like a buffer. It stores 2048 samples (bytes of data)
@@ -19,7 +19,7 @@ class voice2text:
         
         #generate a list of all audio cards/microphones
         mic_list = sr.Microphone.list_microphone_names()
-        
+        print(mic_list)
         #the following loop aims to set the device ID of the mic that
         #we specifically want to use to avoid ambiguity.
         for i, microphone_name in enumerate(mic_list):
@@ -32,24 +32,25 @@ class voice2text:
                         chunk_size = self.chunk_size) as source:
             #wait for a second to let the recognizer adjust the 
             #energy threshold based on the surrounding noise level
-            self.r.adjust_for_ambient_noise(source)
+            # self.r.adjust_for_ambient_noise(source)
             # print "Say Something"
             #listens for the user's input
             audio = self.r.listen(source)
                 
             try:
                 text = self.r.recognize_google(audio)
+
                 print "you said: " + text
                 return text
             
             #error occurs when google could not understand what was saidword2t
             
             except sr.UnknownValueError:
-                # print("Google Speech Recognition could not understand audio")
+                print("Google Speech Recognition could not understand audio")
                 return "X"
             
             except sr.RequestError as e:
-                # print("Could not request results from Google peech Recognition service")
+                print("Could not request results from Google peech Recognition service")
                 return "X"
 
 if __name__ == "__main__":
--- yilun/launch.py
+++ yilun/launch.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+
+import db_wrapper
 import os
 
 
@@ -15,7 +17,25 @@ def hello():
         if os.path.splitext(f)[1].lower() in ('.jpg', '.jpeg'):
             nameList.append(f.split(".")[0])
             pathList.append('/static/'+ f.split(".")[0] + '.jpg')
-        return render_template('Test.html',var1 = nameList, var2 = pathList)
+    return render_template('test.html',var1 = nameList, var2 = pathList)
+
+@app.route('/Video')
+def index():
+    return render_template('Video.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        if frame is not None:
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 
 if __name__ == '__main__':
     app.run()
